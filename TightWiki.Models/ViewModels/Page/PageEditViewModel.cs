d5c84b606a9136d0d483b41841e85681b27db9a4
Milan Chvojka <dev@mago.cz>
2025-08-04 10:25:42 +0200

    Localization of labels on the PageEdit page

--- TightWiki.Models/ViewModels/Page/PageEditViewModel.cs
+++ TightWiki.Models/ViewModels/Page/PageEditViewModel.cs
@@ -7,9 +7,12 @@ namespace TightWiki.Models.ViewModels.Page
         public int Id { get; set; }
 
         [Required(ErrorMessageResourceName = "RequiredAttribute_ValidationError", ErrorMessageResourceType = typeof(Models.Resources.ValTexts))]
+        [Display(Name="Name", ResourceType = typeof(Models.Resources.ViewModels.Page.PageEditViewModel))]
         public string Name { get; set; } = string.Empty;
         public string Navigation { get; set; } = string.Empty;
+        [Display(Name= "Description", ResourceType = typeof(Models.Resources.ViewModels.Page.PageEditViewModel))]
         public string? Description { get; set; } = string.Empty;
+        [Display(Name= "Body", ResourceType = typeof(Models.Resources.ViewModels.Page.PageEditViewModel))]
         public string? Body { get; set; } = string.Empty;
         public List<DataModels.Page> Templates { get; set; } = new();
     }
